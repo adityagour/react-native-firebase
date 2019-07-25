535f4ab9ceebeed10edd97e4accd07e2938dbdef
AdityaGour <adityagourluck@gmail.com>
2019-07-25 12:21:15 +0530

    add firebase config

---
+++ environment/config.js
@@ -0,0 +1,15 @@
+import * as firebase from 'firebase';
+
+// Initialize Firebase
+const firebaseConfig = {
+    apiKey: "AIzaSyCiEPZa0JMJTIBd-LAqcGq1LE0JH7XS1aM",
+    authDomain: "fir-starter-d67c9.firebaseapp.com",
+    databaseURL: "https://fir-starter-d67c9.firebaseio.com",
+    projectId: "fir-starter-d67c9",
+    storageBucket: "",
+    messagingSenderId: "356321789175",
+    appId: "1:356321789175:web:04f1554ffedc6ead"
+  };
+  const firebaseApp = firebase.initializeApp(firebaseConfig);
+
+  export const firebaseAuth = firebaseApp.auth();
